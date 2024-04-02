4a1bb976c3300d804fa7b2807ac9516ece51b8de
vlrpor <v.forchkhidze@gmail.com>
2024-04-02 05:31:00 -0700

    add js types and console log

---
+++ app.js
@@ -0,0 +1,24 @@
+let teamName = "Barca";
+const teamAge = 125;
+const teamPrice = null;
+let teamAvailable = false;
+let teamBrand;
+
+console.log(teamName);
+console.log(teamAge);
+console.log(teamPrice);
+console.log(teamAvailable);
+console.log(teamBrand);
+
+let myName = "Valeri";
+let myHobby = "Swimming";
+const yearOfBirth = 1994;
+let currentYear = 2024;
+
+console.log(myName);
+console.log(myHobby);
+console.log(yearOfBirth);
+console.log(currentYear);
+
+let aboutUser = 'My name is ' + myName + ' I am ' + (currentYear - yearOfBirth) + ' years old' + ' and my hobby is ' + myHobby;
+console.log(aboutUser);
